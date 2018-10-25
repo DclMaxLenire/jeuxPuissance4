a0d97455f2f5ab51143e4e825bda664e1524fbed
dclchrisfrakkass <dcl.chrisl@18pixel.fr>
2018-10-25 14:23:41 +0200

    add v2 table type

---
+++ script2.js
@@ -0,0 +1,17 @@
+// Variable //
+var quiJoue;
+var choixCaseJoueur1;
+var choixCaseJoueur2;
+// Démmarage du jeu //
+// Joueur 1 Joue //
+// Récupération coordonés pions joueur 1 //
+// Verifier si vide //
+// Apparition du pion //
+// Joueur 2 Joue //
+//Récupération coordonés pions joueur 2 //
+// Verifier si vide //
+// Apparition du pion //
+// Analyse du nombre de pion aligné //
+// Si 4 pions alignés //
+// Si plus de pions //
+// Restrat//
