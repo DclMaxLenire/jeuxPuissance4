c7abd70c2ac223f4f9d721249e00523ed86a5326
DclMaxLenire <43648539+DclMaxLenire@users.noreply.github.com>
2018-10-25 16:00:50 +0200

    max update

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // Variable //
+var lancerJeux;
 var quiJoue;
 var choixCaseJoueur1;
 var choixCaseJoueur2;
+var finJeux;
+var relancer;
+var aa = document.getElementById("1:1").addEventListener("click", joue);
+var ab = document.getElementById("1:2").addEventListener("click", joue);
+var ac = document.getElementById("1:3").addEventListener("click", joue);
 // Démmarage du jeu //
 // Joueur 1 Joue //
+function joue(event) {
+    console.log(event.target.id);
+    
+
+
+}
 // Récupération coordonés pions joueur 1 //
 // Verifier si vide //
 // Apparition du pion //
