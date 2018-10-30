0dd366458aae8d1fad12d1d9bf347de6f87630f7
DclMaxLenire <43648539+DclMaxLenire@users.noreply.github.com>
2018-10-30 12:29:31 +0100

    Tout marche

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const debug = true; // mode debug
 var player=1; //1 for Yellow (player1), 2 for Red (player2)
 var row = 6;
+var gameActive = false;
+var turn = 0;
 var jeu = [
 	[0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0],
@@ -11,39 +13,21 @@ var jeu = [
 	[0, 0, 0, 0, 0, 0, 0],
 ];
 
-// function firstFreeRow(col,player){
-// 	for(var i = 0; i<6; i++){
-// 	  if(jeu[i][col]!=0){
-// 		break;
-// 	  }
-// 	}
-// 	jeu[i-1][col] = player;
-// 	return i-1;
-
-
 
 
 ///work/////
-//verify the player
-// function selectColumn(col) {
 
-//	function dropToBottom(row, col) {
-// Start at the bottom of the column, and step up, checking to make sure
-// each position has been filled. If one hasn't, return the empty position.
-// for (var y = 6; y > row; y--) {
-// 	if (jeu[y][col] === 0) {
-// 		return y;
-// 	}
-// }
+function startGame(){
+	if (gameActive == true) return false; 
+	gameActive = true;
+}
 
-/* drawBoard will draw the board - it will update each item to make sure it is the appropriate value */
+
 
 function drawBoard() {
-	// checkForWin(); //check to see if any player has won.
+	if (gameActive) {
 	for (col = 0; col<=6; col++) {
-		for (row=0; row<=6; row++) {
-			//Set the inner HTML of the square (a td) to be a span with the class of 'piece' and 'player' + the value of that 
-			//gameboard piece.  Using CSS, you can style player0, player1 and player2 so that the square will appear correctly.
+		for (row = 0; row<=6; row++) {
 			if (jeu[row][col]==0) { 														//verify if the number in the array is 0
 				document.getElementById("cell"+row+col).style.backgroundColor="white";
 			} else if (jeu[row][col]==1) { 													//verify if the number in the array is 0
@@ -53,186 +37,271 @@ function drawBoard() {
 			}
 		}	
 	}
-}
-/* drop will add a piece to the lowest available column */
-function selectColumn(col) {
-	/* Look for the lowest point in this row that is open */
-	//the opposite of our loop above - as we're going to start from the bottom looking for an open slot
-	for (row=6; row>=0; row--) { //note: we are using row--, which will reduce the value of row by 1, the opposted of ++
-		if (jeu[row][col] == 0) {
-			//set the empty row to the active player's number
-			jeu[row][col] = player;
-			drawBoard(); // draw the board.
-			//change the active players turn:
-			if (player == 1) {
-				player = 2;
-			} else {
-				player = 1;
-			}
-			//there is also a fancy way of doing this call a ternary assignment that looks like this: active_player = (active_player == 1) ? 2 : 1;
-			
-		//display who is the active player
-			//stop looking for empty spaces
-			return true;
-		}
+}}
+
+function displayPlay() {
+	if (gameActive){
+		document.getElementById("colorTurn").innerHTML="Tour : Joueur "+player;
+	}
+	if (player == 1)
+	{
+		document.getElementById("colorTurn").style.color="yellow";
+	} else {
+		document.getElementById("colorTurn").style.color="red";
 	}
 }
 
-
-
-// if (player==1) {    //if player 1 play
-// 	if (jeu[row][col]==0){
-// 		jeu[row][col]=1;
-// 		player=2;
-// 		document.getElementById("colorTurn").innerHTML="Tour : Joueur 2";
-// 		console.log(jeu);}
-// 		else {
-// 			row--;			
-// 			jeu[row][col]=1;
-// 			player=2;
-// 			document.getElementById("colorTurn").innerHTML="Tour : Joueur 2";
-// 			console.log(jeu);}}
-// 			else {			//if player 2 play
-// 				if (jeu[row][col]==0){
-// 					jeu[row][col]=2;
-// 					player=1;
-// 					document.getElementById("colorTurn").innerHTML="Tour : Joueur 1";
-// 					console.log(jeu);}
-// 					else {
-// 						jeu[row][col]=2;
-// 						player=1;
-// 						document.getElementById("colorTurn").innerHTML="Tour : Joueur 2";
-// 						console.log(jeu);
-
-// 					}
-// 			}
-
-// 				placeColor();
-// 			}
-
-
-//to place a color on board
-// function placeColor() {
-// 	for (var row = 0; row < 7; row++) {					//verify if row is between 0 and 7
-// 		for (var col = 0; col < 7; col++) {				//verify if column is between 0 and 7
-// 			if (jeu[row][col]==0) { 														//verify if the number in the array is 0
-// 				document.getElementById("cell"+row+col).style.backgroundColor="white";
-// 			} else if (jeu[row][col]==1) { 													//verify if the number in the array is 0
-// 				document.getElementById("cell"+row+col).style.backgroundColor="yellow";
-// 			} else if (jeu[row][col]==2) { 													//verify if the number in the array is 0
-// 				document.getElementById("cell"+row+col).style.backgroundColor="red";
-// 			}
-			// else if (jeu[row][col]!=0) {
-			// 	for (row =0; row < 7; row--) {
-			// 		document.getElementById("cell"+row+col).style.backgroundColor="yellow";
-			// 	}
-// 			// }
-// 		}
-// 	}  
-// }
-
-
-
-
-var reset = document.getElementById("resetButton");
-resetButton.addEventListener("click",
-function resetGame(){
-	location.reload();
-});
-
-
-
-//////////
-// test elements
-//////////
-
-
-// function jouer_pion(i)
-// {	
-// 	//recherche le nombre de pions déjà joués dans cette colonne :
-// 	n=0;
-// 	for (col=1;col<=6;col++) 
-// 	{
-// 		if (jeu[row][col]!=0) { n++ }
-// 	}
-// 	n=6-n;
-
-// 	if (n==0)
-// 	{
-// 		document.getElementById("colorTurn").innerHTML = 'La colonne est pleine';
-// 	}
-// 	else
-// 	{
-// 		document.getElementById("colorTurn").innerHTML = '&nbsp;';
-// 		if (player==1)
-// 		{ 
-// 			document.getElementById("cell"+row+col).style.backgroundColor="yellow";
-// 			jeu[row][col]=1;
-// 			player=2;
-// 			document.getElementById("colorTurn").innerHTML = 'Couleur du prochain pion joué : ROUGE';	
-// 		}
-// 		else
-// 		{ 
-// 			document.getElementById("cell"+row+col).style.backgroundColor="red";
-// 			jeu[row][col]=1;
-// 			player=1;
-// 			document.getElementById("colorTurn").innerHTML = 'Couleur du prochain pion joué : BLEU';	
-// 		}
-// 	}
-// }
-
-//verify if an cell is taken
-// function checkCol(col) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////
-///																	 ///
-///								OLD VERSION							 ///
-///																	 ///
-////////////////////////////////////////////////////////////////////////
-
-
-// var table = document.getElementById("myTable"),rIndex,cIndex;
-
-// // table rows
-// for(var i = 0; i < table.rows.length; i++)
-// {
-//     // row cells
-//     for(var j = 0; j < table.rows[i].cells.length; j++)
-//     {
-//         table.rows[i].cells[j].onclick = function()
-//         {
-//             rIndex = this.parentElement.rowIndex;
-//             cIndex = this.cellIndex;
-//             console.log("Row : "+rIndex+" , Cell : "+cIndex);
-//             console.log(jeu[rIndex][cIndex]);
-//             jeu[rIndex][cIndex]= 1;
-//             console.log(jeu[rIndex][cIndex]);
-//             jeu.forEach(Number => console.log(Number));
-
-//         };
-//     }
-// }
-// var event;
-// table.addEventListener("click",
-//         function (e) {
-//             console.log(e.target);
-//             event = e.target;
-//         }
-// );
-
+// add a piece to the lowest available column 
+function selectColumn(col) {
+	lookForWin();
+	if (gameActive) {
+		// Look for the lowest point in this row that is open
+		for (row=6; row>=0; row--) {
+			if (jeu[row][col] == 0) {
+				jeu[row][col] = player;
+				drawBoard();
+				//change the active players turn:
+				if (player == 1) {
+					player = 2;
+				} else {
+					player = 1;
+				}
+				displayPlay();
+				
+				//stop looking for empty spaces
+				return true;
+				
+			}
+		}
+	}}
+	
+	// look if a player win and who's winning
+	function lookForWin() {
+		//check horizontal
+		for (i=1; i<=2; i++) {
+			for (col = 0; col <=4; col++) {
+				for (row = 0; row <=6; row++) {
+					if (jeu[row][col] == i) {
+						if ((jeu[row][col+1] == i) && (jeu[row][col+2] == i) && (jeu[row][col+3] == i)) {
+							endGame(i);
+							return true; //the game is ending
+						}
+					}
+				}
+			}
+		}
+		//check vertical
+		for (i=1; i<=2; i++) {
+			for (col = 0; col <=6; col++) {
+				for (row = 0; row <=3; row++) {
+					if (jeu[row][col] == i) {
+						if ((jeu[row+1][col] == i) && (jeu[row+2][col] == i) && (jeu[row+3][col] == i)) {
+							endGame(i); 
+							return true; 
+						}
+					}
+				}
+			}
+		}
+		//chek diagonal direction 1 down rigth//
+		for (i=1; i<=2; i++) {
+			for (col = 0; col <=3; col++) {
+				for (row = 0; row <=3; row++) {
+					if (jeu[row][col] == i) {
+						if ((jeu[row+1][col+1] == i) && (jeu[row+2][col+2] == i) && (jeu[row+3][col+3] == i)) {
+							endGame(i);
+							return true;
+						}
+					}
+				}
+			}
+		}
+		//check diagonal directon 2 up rigth //
+		for (i=1; i<=2; i++) {
+			for (col = 0 ; col <=3; col++) {
+				for (row = 3; row <=6; row++) {
+					if (jeu[row][col] == i) {
+						if ((jeu[row-1][col+1] == i) && (jeu[row-2][col+2] == i) && (jeu[row-3][col+3] == i)) {
+							endGame(i);
+							return true;
+						}
+					}
+				}
+			}		
+		}
+	 };
+	
+	function endGame(winner) {
+		gameActive = false; 
+		document.getElementById('winLose').innerHTML = "Joueur gagnant: joueur " + winner; //set the winning player
+		if (player == 1){
+			document.getElementById("winLose").style.color="red";
+		} else {
+			document.getElementById("winLose").style.color="yellow";
+		} 
+		document.getElementById("colorTurn").style.color="white";
+		document.getElementById("colorTurn").innerHTML="END";
+	}
+	
+	var reset = document.getElementById("resetButton");
+	resetButton.addEventListener("click",
+	function resetGame(){
+		location.reload();
+	});
+	
+	
+	
+	
+	////////////////////////////////////////////////////////////////////////
+	///																	 ///
+	///								OLD VERSION	2						 ///
+	///																	 ///
+	////////////////////////////////////////////////////////////////////////
+	// if (player==1) {    //if player 1 play
+	// 	if (jeu[row][col]==0){
+	// 		jeu[row][col]=1;
+	// 		player=2;
+	// 		document.getElementById("colorTurn").innerHTML="Tour : Joueur 2";
+	// 		console.log(jeu);}
+	// 		else {
+	// 			row--;			
+	// 			jeu[row][col]=1;
+	// 			player=2;
+	// 			document.getElementById("colorTurn").innerHTML="Tour : Joueur 2";
+	// 			console.log(jeu);}}
+	// 			else {			//if player 2 play
+	// 				if (jeu[row][col]==0){
+	// 					jeu[row][col]=2;
+	// 					player=1;
+	// 					document.getElementById("colorTurn").innerHTML="Tour : Joueur 1";
+	// 					console.log(jeu);}
+	// 					else {
+	// 						jeu[row][col]=2;
+	// 						player=1;
+	// 						document.getElementById("colorTurn").innerHTML="Tour : Joueur 2";
+	// 						console.log(jeu);
+	
+	// 					}
+	// 			}
+	
+	// 				placeColor();
+	// 			}
+	
+	
+	//to place a color on board
+	// function placeColor() {
+	// 	for (var row = 0; row < 7; row++) {					//verify if row is between 0 and 7
+	// 		for (var col = 0; col < 7; col++) {				//verify if column is between 0 and 7
+	// 			if (jeu[row][col]==0) { 														//verify if the number in the array is 0
+	// 				document.getElementById("cell"+row+col).style.backgroundColor="white";
+	// 			} else if (jeu[row][col]==1) { 													//verify if the number in the array is 0
+	// 				document.getElementById("cell"+row+col).style.backgroundColor="yellow";
+	// 			} else if (jeu[row][col]==2) { 													//verify if the number in the array is 0
+	// 				document.getElementById("cell"+row+col).style.backgroundColor="red";
+	// 			}
+	// else if (jeu[row][col]!=0) {
+	// 	for (row =0; row < 7; row--) {
+	// 		document.getElementById("cell"+row+col).style.backgroundColor="yellow";
+	// 	}
+	// 			// }
+	// 		}
+	// 	}  
+	// }
+	
+	
+	
+	//////////
+	// test elements
+	//////////
+	
+	
+	// function jouer_pion(i)
+	// {	
+	// 	//recherche le nombre de pions déjà joués dans cette colonne :
+	// 	n=0;
+	// 	for (col=1;col<=6;col++) 
+	// 	{
+	// 		if (jeu[row][col]!=0) { n++ }
+	// 	}
+	// 	n=6-n;
+	
+	// 	if (n==0)
+	// 	{
+	// 		document.getElementById("colorTurn").innerHTML = 'La colonne est pleine';
+	// 	}
+	// 	else
+	// 	{
+	// 		document.getElementById("colorTurn").innerHTML = '&nbsp;';
+	// 		if (player==1)
+	// 		{ 
+	// 			document.getElementById("cell"+row+col).style.backgroundColor="yellow";
+	// 			jeu[row][col]=1;
+	// 			player=2;
+	// 			document.getElementById("colorTurn").innerHTML = 'Couleur du prochain pion joué : ROUGE';	
+	// 		}
+	// 		else
+	// 		{ 
+	// 			document.getElementById("cell"+row+col).style.backgroundColor="red";
+	// 			jeu[row][col]=1;
+	// 			player=1;
+	// 			document.getElementById("colorTurn").innerHTML = 'Couleur du prochain pion joué : BLEU';	
+	// 		}
+	// 	}
+	// }
+	
+	//verify if an cell is taken
+	// function checkCol(col) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	////////////////////////////////////////////////////////////////////////
+	///																	 ///
+	///								OLD VERSION							 ///
+	///																	 ///
+	////////////////////////////////////////////////////////////////////////
+	
+	
+	// var table = document.getElementById("myTable"),rIndex,cIndex;
+	
+	// // table rows
+	// for(var i = 0; i < table.rows.length; i++)
+	// {
+	//     // row cells
+	//     for(var j = 0; j < table.rows[i].cells.length; j++)
+	//     {
+	//         table.rows[i].cells[j].onclick = function()
+	//         {
+	//             rIndex = this.parentElement.rowIndex;
+	//             cIndex = this.cellIndex;
+	//             console.log("Row : "+rIndex+" , Cell : "+cIndex);
+	//             console.log(jeu[rIndex][cIndex]);
+	//             jeu[rIndex][cIndex]= 1;
+	//             console.log(jeu[rIndex][cIndex]);
+	//             jeu.forEach(Number => console.log(Number));
+	
+	//         };
+	//     }
+	// }
+	// var event;
+	// table.addEventListener("click",
+	//         function (e) {
+	//             console.log(e.target);
+	//             event = e.target;
+	//         }
+	// );
+	
+	
